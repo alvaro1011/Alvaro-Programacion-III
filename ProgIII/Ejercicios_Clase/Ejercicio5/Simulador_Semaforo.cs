fee3540b875ec8c15c87926d1d2c3a94d6c2f019
alvaro1011 <alvaromurcia347@gmail.com>
2026-03-01 22:47:58 -0500

    uptade 9

---
+++ ProgIII/Ejercicios_Clase/Ejercicio5/Simulador_Semaforo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgIII.Ejercicios_Clase.Ejercicio5
+{
+    public class SemaforoInteligente
+    {
+        public string ColorActual;
+
+       public SemaforoInteligente(string color)
+        {
+            ColorActual = color.ToLower();
+
+        }
+
+        public void MostrarAccion()
+        {
+            Console.WriteLine("");
+
+            if (ColorActual == "verde")
+            {
+                Console.WriteLine("Accion: Sigue adelante");
+            }
+            else if (ColorActual == "amarillo")
+            {
+                Console.WriteLine("Accion: Preparate para frenar");
+            }
+            else if (ColorActual == "rojo")
+            {
+                Console.WriteLine("Accion: ¡Detente!");
+            }
+            else
+            {
+                Console.WriteLine("Color no reconocido. Intente de nuevo.");
+            }
+        }
+    }
+}
